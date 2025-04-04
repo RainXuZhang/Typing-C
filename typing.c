9338cc6dd53f96d7c78f100301ba5e954d6e5196
Rain <rainxuzhang@gmail.com>
2025-04-04 11:42:33 -0700

    Initial commit
    Initial commit. Do not forget to include the header file.

---
+++ typing.c
@@ -0,0 +1,9 @@
+#include <typing.h>
+
+int main()
+{
+    I();space();l();o();v();e();space();m();u();s();i();c();exclamation();endline();
+    I();space();l();o();v();e();space();c();o();d();i();n();g();exclamation();endline();
+    
+    return (0);
+}
---
+++ typing.h
@@ -0,0 +1,690 @@
+#ifndef typing_h
+#define typing_h
+#include <stdio.h>
+#include <unistd.h>
+
+// Lowercase letters
+void a(void);
+void b(void);
+void c(void);
+void d(void);
+void e(void);
+void f(void);
+void g(void);
+void h(void);
+void i(void);
+void j(void);
+void k(void);
+void l(void);
+void m(void);
+void n(void);
+void o(void);
+void p(void);
+void q(void);
+void r(void);
+void s(void);
+void t(void);
+void u(void);
+void v(void);
+void w(void);
+void x(void);
+void y(void);
+void z(void);
+
+// Uppercase letters
+void A(void);
+void B(void);
+void C(void);
+void D(void);
+void E(void);
+void F(void);
+void G(void);
+void H(void);
+void I(void);
+void J(void);
+void K(void);
+void L(void);
+void M(void);
+void N(void);
+void O(void);
+void P(void);
+void Q(void);
+void R(void);
+void S(void);
+void T(void);
+void U(void);
+void V(void);
+void W(void);
+void X(void);
+void Y(void);
+void Z(void);
+
+// Numbers
+void zero(void);
+void one(void);
+void two(void);
+void three(void);
+void four(void);
+void five(void);
+void six(void);
+void seven(void);
+void eight(void);
+void nine(void);
+
+// Symbols
+void exclamation(void);
+void at(void);
+void hash(void);
+void dollar(void);
+void percent(void);
+void caret(void);
+void ampersand(void);
+void asterisk(void);
+void leftparen(void);
+void rightparen(void);
+void dash(void);
+void underscore(void);
+void plus(void);
+void equals(void);
+void leftbrace(void);
+void rightbrace(void);
+void leftbracket(void);
+void rightbracket(void);
+void verticalbar(void);
+void backslash(void);
+void semicolon(void);
+void colon(void);
+void singlequote(void);
+void doublequote(void);
+void comma(void);
+void period(void);
+void slash(void);
+void question(void);
+void tilde(void);
+void leftangle(void);
+void rightangle(void);
+void backtick(void);
+
+// Other
+void space(void);
+void endline(void);
+
+// Declare functions for each character
+void a()
+{
+    usleep(300000);
+    printf("a");
+    fflush(stdout);
+}
+void b()
+{
+    usleep(500000);
+    printf("b");
+    fflush(stdout);
+}
+void c()
+{
+    usleep(500000);
+    printf("c");
+    fflush(stdout);
+}
+void d()
+{
+    usleep(300000);
+    printf("d");
+    fflush(stdout);
+}
+void e()
+{
+    usleep(400000);
+    printf("e");
+    fflush(stdout);
+}
+void f()
+{
+    usleep(200000);
+    printf("f");
+    fflush(stdout);
+}
+void g()
+{
+    usleep(300000);
+    printf("g");
+    fflush(stdout);
+}
+void h()
+{
+    usleep(300000);
+    printf("h");
+    fflush(stdout);
+}
+void i()
+{
+    usleep(400000);
+    printf("i");
+    fflush(stdout);
+}
+void j()
+{
+    usleep(200000);
+    printf("j");
+    fflush(stdout);
+}
+void k()
+{
+    usleep(300000);
+    printf("k");
+    fflush(stdout);
+}
+void l()
+{
+    usleep(300000);
+    printf("l");
+    fflush(stdout);
+}
+void m()
+{
+    usleep(500000);
+    printf("m");
+    fflush(stdout);
+}
+void n()
+{
+    usleep(500000);
+    printf("n");
+    fflush(stdout);
+}
+void o()
+{
+    usleep(400000);
+    printf("o");
+    fflush(stdout);
+}
+void p()
+{
+    usleep(400000);
+    printf("p");
+    fflush(stdout);
+}
+void q()
+{
+    usleep(400000);
+    printf("q");
+    fflush(stdout);
+}
+void r()
+{
+    usleep(400000);
+    printf("r");
+    fflush(stdout);
+}
+void s()
+{
+    usleep(300000);
+    printf("s");
+    fflush(stdout);
+}
+void t()
+{
+    usleep(400000);
+    printf("t");
+    fflush(stdout);
+}
+void u()
+{
+    usleep(400000);
+    printf("u");
+    fflush(stdout);
+}
+void v()
+{
+    usleep(500000);
+    printf("v");
+    fflush(stdout);
+}
+void w()
+{
+    usleep(400000);
+    printf("w");
+    fflush(stdout);
+}
+void x()
+{
+    usleep(500000);
+    printf("x");
+    fflush(stdout);
+}
+void y()
+{
+    usleep(400000);
+    printf("y");
+    fflush(stdout);
+}
+void z()
+{
+    usleep(500000);
+    printf("z");
+    fflush(stdout);
+}
+void A()
+{
+    usleep(300000);
+    printf("A");
+    fflush(stdout);
+}
+void B()
+{
+    usleep(500000);
+    printf("B");
+    fflush(stdout);
+}
+void C()
+{
+    usleep(500000);
+    printf("C");
+    fflush(stdout);
+}
+void D()
+{
+    usleep(300000);
+    printf("D");
+    fflush(stdout);
+}
+void E()
+{
+    usleep(400000);
+    printf("E");
+    fflush(stdout);
+}
+void F()
+{
+    usleep(200000);
+    printf("F");
+    fflush(stdout);
+}
+void G()
+{
+    usleep(300000);
+    printf("G");
+    fflush(stdout);
+}
+void H()
+{
+    usleep(300000);
+    printf("H");
+    fflush(stdout);
+}
+void I()
+{
+    usleep(400000);
+    printf("I");
+    fflush(stdout);
+}
+void J()
+{
+    usleep(200000);
+    printf("J");
+    fflush(stdout);
+}
+void K()
+{
+    usleep(300000);
+    printf("K");
+    fflush(stdout);
+}
+void L()
+{
+    usleep(300000);
+    printf("L");
+    fflush(stdout);
+}
+void M()
+{
+    usleep(500000);
+    printf("M");
+    fflush(stdout);
+}
+void N()
+{
+    usleep(500000);
+    printf("N");
+    fflush(stdout);
+}
+void O()
+{
+    usleep(400000);
+    printf("O");
+    fflush(stdout);
+}
+void P()
+{
+    usleep(400000);
+    printf("P");
+    fflush(stdout);
+}
+void Q()
+{
+    usleep(400000);
+    printf("Q");
+    fflush(stdout);
+}
+void R()
+{
+    usleep(400000);
+    printf("R");
+    fflush(stdout);
+}
+void S()
+{
+    usleep(300000);
+    printf("S");
+    fflush(stdout);
+}
+void T()
+{
+    usleep(400000);
+    printf("T");
+    fflush(stdout);
+}
+void U()
+{
+    usleep(400000);
+    printf("U");
+    fflush(stdout);
+}
+void V()
+{
+    usleep(500000);
+    printf("V");
+    fflush(stdout);
+}
+void W()
+{
+    usleep(400000);
+    printf("W");
+    fflush(stdout);
+}
+void X()
+{
+    usleep(500000);
+    printf("X");
+    fflush(stdout);
+}
+void Y()
+{
+    usleep(400000);
+    printf("Y");
+    fflush(stdout);
+}
+void Z()
+{
+    usleep(500000);
+    printf("Z");
+    fflush(stdout);
+}
+void zero()
+{
+    usleep(300000);
+    printf("0");
+    fflush(stdout);
+}
+void one()
+{
+    usleep(300000);
+    printf("1");
+    fflush(stdout);
+}
+void two()
+{
+    usleep(300000);
+    printf("2");
+    fflush(stdout);
+}
+void three()
+{
+    usleep(300000);
+    printf("3");
+    fflush(stdout);
+}
+void four()
+{
+    usleep(300000);
+    printf("4");
+    fflush(stdout);
+}
+void five()
+{
+    usleep(300000);
+    printf("5");
+    fflush(stdout);
+}
+void six()
+{
+    usleep(300000);
+    printf("6");
+    fflush(stdout);
+}
+void seven()
+{
+    usleep(300000);
+    printf("7");
+    fflush(stdout);
+}
+void eight()
+{
+    usleep(300000);
+    printf("8");
+    fflush(stdout);
+}
+void nine()
+{
+    usleep(300000);
+    printf("9");
+    fflush(stdout);
+}
+void exclamation()
+{
+    usleep(300000);
+    printf("!");
+    fflush(stdout);
+}
+void at()
+{
+    usleep(300000);
+    printf("@");
+    fflush(stdout);
+}
+void hash()
+{
+    usleep(300000);
+    printf("#");
+    fflush(stdout);
+}
+void dollar()
+{
+    usleep(300000);
+    printf("$");
+    fflush(stdout);
+}
+void percent()
+{
+    usleep(300000);
+    printf("%%");
+    fflush(stdout);
+}
+void caret()
+{
+    usleep(300000);
+    printf("^");
+    fflush(stdout);
+}
+void ampersand()
+{
+    usleep(300000);
+    printf("&");
+    fflush(stdout);
+}
+void asterisk()
+{
+    usleep(300000);
+    printf("*");
+    fflush(stdout);
+}
+void leftparen()
+{
+    usleep(300000);
+    printf("(");
+    fflush(stdout);
+}
+void rightparen()
+{
+    usleep(300000);
+    printf(")");
+    fflush(stdout);
+}
+void dash()
+{
+    usleep(300000);
+    printf("-");
+    fflush(stdout);
+}
+void underscore()
+{
+    usleep(300000);
+    printf("_");
+    fflush(stdout);
+}
+void plus()
+{
+    usleep(300000);
+    printf("+");
+    fflush(stdout);
+}
+void equals()
+{
+    usleep(300000);
+    printf("=");
+    fflush(stdout);
+}
+void leftbrace()
+{
+    usleep(300000);
+    printf("{");
+    fflush(stdout);
+}
+void rightbrace()
+{
+    usleep(300000);
+    printf("}");
+    fflush(stdout);
+}
+void leftbracket()
+{
+    usleep(300000);
+    printf("[");
+    fflush(stdout);
+}
+void rightbracket()
+{
+    usleep(300000);
+    printf("]");
+    fflush(stdout);
+}
+void verticalbar()
+{
+    usleep(300000);
+    printf("|");
+    fflush(stdout);
+}
+void backslash()
+{
+    usleep(300000);
+    printf("\\");
+    fflush(stdout);
+}
+void semicolon()
+{
+    usleep(300000);
+    printf(";");
+    fflush(stdout);
+}
+void colon()
+{
+    usleep(300000);
+    printf(":");
+    fflush(stdout);
+}
+void singlequote()
+{
+    usleep(300000);
+    printf("'");
+    fflush(stdout);
+}
+void doublequote()
+{
+    usleep(300000);
+    printf("\"");
+    fflush(stdout);
+}
+void comma()
+{
+    usleep(300000);
+    printf(",");
+    fflush(stdout);
+}
+void period()
+{
+    usleep(300000);
+    printf(".");
+    fflush(stdout);
+}
+void slash()
+{
+    usleep(300000);
+    printf("/");
+    fflush(stdout);
+}
+void question()
+{
+    usleep(300000);
+    printf("?");
+    fflush(stdout);
+}
+void tilde()
+{
+    usleep(300000);
+    printf("~");
+    fflush(stdout);
+}
+void leftangle()
+{
+    usleep(300000);
+    printf("<");
+    fflush(stdout);
+}
+void rightangle()
+{
+    usleep(300000);
+    printf(">");
+    fflush(stdout);
+}
+void backtick()
+{
+    usleep(300000);
+    printf("`");
+    fflush(stdout);
+}
+void space()
+{
+    usleep(300000);
+    printf(" ");
+    fflush(stdout);
+}
+void endline()
+{
+    usleep(300000);
+    printf("\n");
+    fflush(stdout);
+}
+
+#endif
