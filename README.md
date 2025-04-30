6dcb1e9727fc8748643e18f35f428b32ba0df94f
Rain <rainxuzhang@gmail.com>
2025-04-30 13:44:38 -0700

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# TypingC
-A program for outputting text as if it were typed, one at a time, instead of all at once. Contains all letters and symbols on the keyboard and more! 
+# Typing-C
+
+# User guide (Linux):
+# Step 1:
+Download the typing.h file.
+
+# Step 2:
+Open your terminal. Go to the file's folder and do `sudo mv typing.c /usr/include`.
+
+# Step 3:
+Create a .c file. At the top, add `#include <typing.h>`.
+Then type as follows:
+`int main()
+{
+  H();i();exclamation();endline();
+  return(0);
+}`
+Save.
+
+# Step 4:
+Compile it using `gcc typing.c -o typing`.
+
+# Step 5:
+Run the file. Voila! It types out `Hi!`, and then adds a new line.
+
+# Character Guide:
+
+Available words:
+Lowercase alphabet:
+a (a); b (b); c (c); d (d); e (e);
+f (f); g (g); h (h); i (i); j (j);
+k (k); l (l); m (m); n (n); o (o);
+p (p); q (q); r (r); s (s); t (t);
+u (u); v (v); w (w); x (x); y (y);
+z (z);
+
+Uppercase alphabet:
+A (A); B (B); C (C); D (D); E (E);
+F (F); G (G); H (H); I (I); J (J);
+K (K); L (L); M (M); N (N); O (O);
+P (P); Q (Q); R (R); S (S); T (T);
+U (U); V (V); W (W); X (X); Y (Y);
+Z (Z);
+
+Numbers:
+zero (0); one (1); two (2);
+three (3); four (4); five (5);
+six (6); seven (7);  eight (8);
+nine (9);
+
+Symbols:
+bang (!); at (@); hash (#);
+dollar ($); percent (%); caret (^);
+ampersand (&); asterisk (*);
+leftparen (() ; rightparen ());
+dash (-); underscore (_);
+plus (+); equals (=);
+leftbrace ({); rightbrace (});
+leftbracket ([); rightbracket (]);
+bar (|); backslash (\);
+semicolon (;); colon (:);
+doublequote ("); comma (,);
+period (.); slash (/);
+leftangle (<); rightangle (>);
+backtick (`); tilde (~);
+question (?);
+
+Other:
+space ( ); endline (\n);
---
+++ typing.h
@@ -0,0 +1,710 @@
+/************************************
+ * typing.h                         *
+ * --------                         *
+ *                                  *
+ * This file is the header file for *
+ * the typing.c file. It contains   *
+ * all functions and #defines.      *
+ ***********************************/
+
+
+
+
+
+
+ #ifndef typing_h
+ #define typing_h
+ #include <stdio.h>
+ #include <unistd.h>
+ 
+ 
+ // Create lowercase alphabet functions
+ void a(void)
+ {
+     usleep(300000);
+     printf("a");
+     fflush(stdout);
+ }
+ void b(void)
+ {
+     usleep(500000);
+     printf("b");
+     fflush(stdout);
+ }
+ void c(void)
+ {
+     usleep(500000);
+     printf("c");
+     fflush(stdout);
+ }
+ void d(void)
+ {
+     usleep(300000);
+     printf("d");
+     fflush(stdout);
+ }
+ void e(void)
+ {
+     usleep(400000);
+     printf("e");
+     fflush(stdout);
+ }
+ void f(void)
+ {
+     usleep(200000);
+     printf("f");
+     fflush(stdout);
+ }
+ void g(void)
+ {
+     usleep(300000);
+     printf("g");
+     fflush(stdout);
+ }
+ void h(void)
+ {
+     usleep(300000);
+     printf("h");
+     fflush(stdout);
+ }
+ void i(void)
+ {
+     usleep(400000);
+     printf("i");
+     fflush(stdout);
+ }
+ void j(void)
+ {
+     usleep(200000);
+     printf("j");
+     fflush(stdout);
+ }
+ void k(void)
+ {
+     usleep(300000);
+     printf("k");
+     fflush(stdout);
+ }
+ void l(void)
+ {
+     usleep(300000);
+     printf("l");
+     fflush(stdout);
+ }
+ void m(void)
+ {
+     usleep(500000);
+     printf("m");
+     fflush(stdout);
+ }
+ void n(void)
+ {
+     usleep(500000);
+     printf("n");
+     fflush(stdout);
+ }
+ void o(void)
+ {
+     usleep(400000);
+     printf("o");
+     fflush(stdout);
+ }
+ void p(void)
+ {
+     usleep(400000);
+     printf("p");
+     fflush(stdout);
+ }
+ void q(void)
+ {
+     usleep(400000);
+     printf("q");
+     fflush(stdout);
+ }
+ void r(void)
+ {
+     usleep(400000);
+     printf("r");
+     fflush(stdout);
+ }
+ void s(void)
+ {
+     usleep(300000);
+     printf("s");
+     fflush(stdout);
+ }
+ void t(void)
+ {
+     usleep(400000);
+     printf("t");
+     fflush(stdout);
+ }
+ void u(void)
+ {
+     usleep(400000);
+     printf("u");
+     fflush(stdout);
+ }
+ void v(void)
+ {
+     usleep(500000);
+     printf("v");
+     fflush(stdout);
+ }
+ void w(void)
+ {
+     usleep(400000);
+     printf("w");
+     fflush(stdout);
+ }
+ void x(void)
+ {
+     usleep(500000);
+     printf("x");
+     fflush(stdout);
+ }
+ void y(void)
+ {
+     usleep(400000);
+     printf("y");
+     fflush(stdout);
+ }
+ void z(void)
+ {
+     usleep(500000);
+     printf("z");
+     fflush(stdout);
+ }
+ 
+ // Create uppercase alphabet functions
+ void A(void)
+ {
+     usleep(300000);
+     printf("A");
+     fflush(stdout);
+ }
+ void B(void)
+ {
+     usleep(500000);
+     printf("B");
+     fflush(stdout);
+ }
+ void C(void)
+ {
+     usleep(500000);
+     printf("C");
+     fflush(stdout);
+ }
+ void D(void)
+ {
+     usleep(300000);
+     printf("D");
+     fflush(stdout);
+ }
+ void E(void)
+ {
+     usleep(400000);
+     printf("E");
+     fflush(stdout);
+ }
+ void F(void)
+ {
+     usleep(200000);
+     printf("F");
+     fflush(stdout);
+ }
+ void G(void)
+ {
+     usleep(300000);
+     printf("G");
+     fflush(stdout);
+ }
+ void H(void)
+ {
+     usleep(300000);
+     printf("H");
+     fflush(stdout);
+ }
+ void I(void)
+ {
+     usleep(400000);
+     printf("I");
+     fflush(stdout);
+ }
+ void J(void)
+ {
+     usleep(200000);
+     printf("J");
+     fflush(stdout);
+ }
+ void K(void)
+ {
+     usleep(300000);
+     printf("K");
+     fflush(stdout);
+ }
+ void L(void)
+ {
+     usleep(300000);
+     printf("L");
+     fflush(stdout);
+ }
+ void M(void)
+ {
+     usleep(500000);
+     printf("M");
+     fflush(stdout);
+ }
+ void N(void)
+ {
+     usleep(500000);
+     printf("N");
+     fflush(stdout);
+ }
+ void O(void)
+ {
+     usleep(400000);
+     printf("O");
+     fflush(stdout);
+ }
+ void P(void)
+ {
+     usleep(400000);
+     printf("P");
+     fflush(stdout);
+ }
+ void Q(void)
+ {
+     usleep(400000);
+     printf("Q");
+     fflush(stdout);
+ }
+ void R(void)
+ {
+     usleep(400000);
+     printf("R");
+     fflush(stdout);
+ }
+ void S(void)
+ {
+     usleep(300000);
+     printf("S");
+     fflush(stdout);
+ }
+ void T(void)
+ {
+     usleep(400000);
+     printf("T");
+     fflush(stdout);
+ }
+ void U(void)
+ {
+     usleep(400000);
+     printf("U");
+     fflush(stdout);
+ }
+ void V(void)
+ {
+     usleep(500000);
+     printf("V");
+     fflush(stdout);
+ }
+ void W(void)
+ {
+     usleep(400000);
+     printf("W");
+     fflush(stdout);
+ }
+ void X(void)
+ {
+     usleep(500000);
+     printf("X");
+     fflush(stdout);
+ }
+ void Y(void)
+ {
+     usleep(400000);
+     printf("Y");
+     fflush(stdout);
+ }
+ void Z(void)
+ {
+     usleep(500000);
+     printf("Z");
+     fflush(stdout);
+ }
+ 
+ // Create number functions
+ void zero(void)
+ {
+     usleep(300000);
+     printf("0");
+     fflush(stdout);
+ }
+ void one(void)
+ {
+     usleep(300000);
+     printf("1");
+     fflush(stdout);
+ }
+ void two(void)
+ {
+     usleep(300000);
+     printf("2");
+     fflush(stdout);
+ }
+ void three(void)
+ {
+     usleep(300000);
+     printf("3");
+     fflush(stdout);
+ }
+ void four(void)
+ {
+     usleep(300000);
+     printf("4");
+     fflush(stdout);
+ }
+ void five(void)
+ {
+     usleep(300000);
+     printf("5");
+     fflush(stdout);
+ }
+ void six(void)
+ {
+     usleep(300000);
+     printf("6");
+     fflush(stdout);
+ }
+ void seven(void)
+ {
+     usleep(300000);
+     printf("7");
+     fflush(stdout);
+ }
+ void eight(void)
+ {
+     usleep(300000);
+     printf("8");
+     fflush(stdout);
+ }
+ void nine(void)
+ {
+     usleep(300000);
+     printf("9");
+     fflush(stdout);
+ }
+ 
+ // Create symbol functions
+ void bang(void)
+ {
+     usleep(300000);
+     printf("!");
+     fflush(stdout);
+ }
+ void at(void)
+ {
+     usleep(300000);
+     printf("@");
+     fflush(stdout);
+ }
+ void hash(void)
+ {
+     usleep(300000);
+     printf("#");
+     fflush(stdout);
+ }
+ void dollar(void)
+ {
+     usleep(300000);
+     printf("$");
+     fflush(stdout);
+ }
+ void percent(void)
+ {
+     usleep(300000);
+     printf("%%");
+     fflush(stdout);
+ }
+ void caret(void)
+ {
+     usleep(300000);
+     printf("^");
+     fflush(stdout);
+ }
+ void ampersand(void)
+ {
+     usleep(300000);
+     printf("&");
+     fflush(stdout);
+ }
+ void asterisk(void)
+ {
+     usleep(300000);
+     printf("*");
+     fflush(stdout);
+ }
+ void leftparen(void)
+ {
+     usleep(300000);
+     printf("(");
+     fflush(stdout);
+ }
+ void rightparen(void)
+ {
+     usleep(300000);
+     printf(")");
+     fflush(stdout);
+ }
+ void dash(void)
+ {
+     usleep(300000);
+     printf("-");
+     fflush(stdout);
+ }
+ void underscore(void)
+ {
+     usleep(300000);
+     printf("_");
+     fflush(stdout);
+ }
+ void plus(void)
+ {
+     usleep(300000);
+     printf("+");
+     fflush(stdout);
+ }
+ void equals(void)
+ {
+     usleep(300000);
+     printf("=");
+     fflush(stdout);
+ }
+ void leftbrace(void)
+ {
+     usleep(300000);
+     printf("{");
+     fflush(stdout);
+ }
+ void rightbrace(void)
+ {
+     usleep(300000);
+     printf("}");
+     fflush(stdout);
+ }
+ void leftbracket(void)
+ {
+     usleep(300000);
+     printf("[");
+     fflush(stdout);
+ }
+ void rightbracket(void)
+ {
+     usleep(300000);
+     printf("]");
+     fflush(stdout);
+ }
+ void bar(void)
+ {
+     usleep(300000);
+     printf("|");
+     fflush(stdout);
+ }
+ void backslash(void)
+ {
+     usleep(300000);
+     printf("\\");
+     fflush(stdout);
+ }
+ void semicolon(void)
+ {
+     usleep(300000);
+     printf(";");
+     fflush(stdout);
+ }
+ void colon(void)
+ {
+     usleep(300000);
+     printf(":");
+     fflush(stdout);
+ }
+ void singlequote(void)
+ {
+     usleep(300000);
+     printf("'");
+     fflush(stdout);
+ }
+ void doublequote(void)
+ {
+     usleep(300000);
+     printf("\"");
+     fflush(stdout);
+ }
+ void comma(void)
+ {
+     usleep(300000);
+     printf(",");
+     fflush(stdout);
+ }
+ void period(void)
+ {
+     usleep(300000);
+     printf(".");
+     fflush(stdout);
+ }
+ void slash(void)
+ {
+     usleep(300000);
+     printf("/");
+     fflush(stdout);
+ }
+ void question(void)
+ {
+     usleep(300000);
+     printf("?");
+     fflush(stdout);
+ }
+ void tilde(void)
+ {
+     usleep(300000);
+     printf("~");
+     fflush(stdout);
+ }
+ void leftangle(void)
+ {
+     usleep(300000);
+     printf("<");
+     fflush(stdout);
+ }
+ void rightangle(void)
+ {
+     usleep(300000);
+     printf(">");
+     fflush(stdout);
+ }
+ void backtick(void)
+ {
+     usleep(300000);
+     printf("`");
+     fflush(stdout);
+ }
+ void space(void)
+ {
+     usleep(300000);
+     printf(" ");
+     fflush(stdout);
+ }
+ void endline(void)
+ {
+     usleep(300000);
+     printf("\n");
+     fflush(stdout);
+ }
+ 
+ // Define lowercase alphabet
+ #define a a();
+ #define b b();
+ #define c c();
+ #define d d();
+ #define e e();
+ #define f f();
+ #define g g();
+ #define h h();
+ #define i i();
+ #define j j();
+ #define k k();
+ #define l l();
+ #define m m();
+ #define n n();
+ #define o o();
+ #define p p();
+ #define q q();
+ #define r r();
+ #define s s();
+ #define t t();
+ #define u u();
+ #define v v();
+ #define w w();
+ #define x x();
+ #define y y();
+ #define z z();
+ 
+ // Define uppercase alphabet
+ #define A A();
+ #define B B();
+ #define C C();
+ #define D D();
+ #define E E();
+ #define F F();
+ #define G G();
+ #define H H();
+ #define I I();
+ #define J J();
+ #define K K();
+ #define L L();
+ #define M M();
+ #define N N();
+ #define O O();
+ #define P P();
+ #define Q Q();
+ #define R R();
+ #define S S();
+ #define T T();
+ #define U U();
+ #define V V();
+ #define W W();
+ #define X X();
+ #define Y Y();
+ #define Z Z();
+ 
+ // Define numbers
+ #define zero zero();
+ #define one one();
+ #define two two();
+ #define three three();
+ #define four four();
+ #define five five();
+ #define six six();
+ #define seven seven();
+ #define eight eight();
+ #define nine nine();
+ 
+ // Define symbols
+ #define bang bang();
+ #define at at();
+ #define hash hash();
+ #define dollar dollar();
+ #define percent percent();
+ #define caret caret();
+ #define ampersand ampersand();
+ #define asterisk asterisk();
+ #define leftparen leftparen();
+ #define rightparen rightparen();
+ #define dash dash();
+ #define underscore underscore();
+ #define plus plus();
+ #define equals equals();
+ 
+ //Define others
+ #define space space();
+ #define newline newline();
+ #define leftbrace leftbrace();
+ #define rightbrace rightbrace();
+ #define leftbracket leftbracket();
+ #define rightbracket rightbracket();
+ #define bar bar();
+ #define backslash backslash();
+ #define semicolon semicolon();
+ #define colon colon();
+ #define doublequote doublequote();
+ #define comma comma();
+ #define period period();
+ #define slash slash();
+ #define question question();
+ #define tilde tilde();
+ #define leftangle leftangle();
+ #define rightangle rightangle();
+ #define backtick backtick();
+ 
+ #endif
