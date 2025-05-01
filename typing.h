3a19e66e1ebce45159083c1cda46f13365c64c07
RainXuZhang <rainxuzhang@gmail.com>
2025-05-01 12:08:02 -0700

    Improved comments & support. Clarified the code for easier modifying.

--- typing.h
+++ typing.h
@@ -1,14 +1,59 @@
-/************************************
- * Typing-C                         *
- * File: typing.h                   *
- * --------                         *
- *                                  *
- * This file is the header file for *
- * the typing.c file. It contains   *
- * all functions and #defines.      *
- ***********************************/
+/**************************************
+ * Typing-C                           *
+ * File: typing.h                     *
+ * --------                           *
+ *                                    *
+ * This file is the header file,      *
+ * which contains the functions. Do   *
+ * not edit this file unless you      *
+ * know what you are doing. Make sure *
+ * to #include this file.             *
+ *                                    *
+ * Available words:                   *
+ * Lowercase alphabet:                *
+ * a (a); b (b); c (c); d (d); e (e); *
+ * f (f); g (g); h (h); i (i); j (j); *
+ * k (k); l (l); m (m); n (n); o (o); *
+ * p (p); q (q); r (r); s (s); t (t); *
+ * u (u); v (v); w (w); x (x); y (y); *
+ * z (z);                             *
+ *                                    *
+ * Uppercase alphabet:                *
+ * A (A); B (B); C (C); D (D); E (E); *
+ * F (F); G (G); H (H); I (I); J (J); *
+ * K (K); L (L); M (M); N (N); O (O); *
+ * P (P); Q (Q); R (R); S (S); T (T); *
+ * U (U); V (V); W (W); X (X); Y (Y); *
+ * Z (Z);                             *
+ *                                    *
+ * Numbers:                           *
+ * zero (0); one (1); two (2);        *
+ * three (3); four (4); five (5);     *
+ * six (6); seven (7);  eight (8);    *
+ * nine (9);                          *
+ *                                    *
+ * Symbols:                           *
+ * bang (!); at (@); hash (#);        *
+ * dollar ($); percent (%); caret (^);*
+ * ampersand (&); asterisk (*);       *
+ * leftparen (() ; rightparen ());    *
+ * dash (-); underscore (_);          *
+ * plus (+); equals (=);              *
+ * leftbrace ({); rightbrace (});     *
+ * leftbracket ([); rightbracket (]); *
+ * bar (|); backslash (\);            *
+ * semicolon (;); colon (:);          *
+ * doublequote ("); comma (,);        *
+ * period (.); slash (/);             *
+ * leftangle (<); rightangle (>);     *
+ * backtick (`); tilde (~);           *
+ * question (?);                      *
+ *                                    *
+ * Other:                             *
+ * space ( ); endline (\n);           *
+ *************************************/
 
- 
+
  #ifndef typing_h
  #define typing_h
  #include <stdio.h>
